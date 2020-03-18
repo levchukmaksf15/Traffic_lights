50847958895694c3dc4defa71de9466a087fee2c
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-18 19:50:57 +0200

    First commiting

---
+++ app/src/main/java/com/non_zero/trafic_lights/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.non_zero.trafic_lights
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

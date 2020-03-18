ea79d260c58c2e3a28ee137660ff399183932ce0
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-18 22:18:43 +0200

    There are 3 buttons for 3 colors of background of the program. You can change background with each of these button and name of color is going to be showed in the textWiev in the bottom of the layout

--- app/src/main/java/com/non_zero/trafic_lights/MainActivity.kt
+++ app/src/main/java/com/non_zero/trafic_lights/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.non_zero.trafic_lights
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        red_button.setOnClickListener {
+            textView.text = resources.getText(R.string.red_button_Text)
+            root_layout.setBackgroundColor(ContextCompat.getColor(this, R.color.redColor))
+        }
+
+        yellow_button.setOnClickListener {
+            textView.setText(R.string.yellow_button_Text)
+            root_layout.setBackgroundColor(ContextCompat.getColor(this, R.color.yellowColor))
+        }
+
+        green_button.setOnClickListener {
+            textView.text = resources.getText(R.string.green_button_Text)
+            root_layout.setBackgroundColor(Color.GREEN)
+        }
     }
 }
